46c5d04fa91b54fe75f444c202f47a55c469944e
Bhubanesh Pinani <shubhamsinha2050@gmail.com>
2020-08-15 15:54:24 +0530

    First commit, comments service

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const axios = require('axios');
+const cors = require('cors')
+
+const app = express()
+app.use(bodyParser.json());
+app.use(cors());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res)=>{
+    res.send(commentsByPostId[req.params.id] || []);
+})
+
+app.post('/posts/:id/comments', async (req, res)=>{
+    const commentId = randomBytes(4).toString('hex');
+    const {content} = req.body;
+
+    const comments = commentsByPostId[req.params.id] || [];
+    comments.push({id: commentId, content});
+    commentsByPostId[req.params.id] = comments;  
+    res.status(201).send(comments);
+
+    await axios.post('http://localhost:4005/events', {
+        'type': "CommentCreate",
+        'data': {
+            "postId": req.params.id,
+            "id": commentId,
+            "comment": content
+        }
+    })
+})
+
+app.post('/events', (req, res) => {
+    console.log(req.body);
+});
+
+app.listen(4001, ()=>{
+    console.log("comments running on port 4001");
+})
